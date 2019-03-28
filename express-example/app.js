69d3070792fd3edb5c9da920291c725da196d993
Anssi Kinnunen <anssi.kinnunen01@gmail.com>
2019-03-28 22:48:08 +0200

    Add todos.json and comments

--- express-example/app.js
+++ express-example/app.js
@@ -1,7 +1,24 @@
+const fs = require("fs");
+const path = require("path");
 const express = require("express");
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => res.send("Hello World!"));
+// reading from fs is easy
+// fs.readFileSync does exactly what it promises: it reads a file synchronously
+// most fs functions expose *sync which used to be good, but nowadays
+// we can use async-functions to use promise-versions asynchronously:
+//
+// const { promisify } = require('util');
+// async function readTodos() {
+//   return await promisify(
+//     fs.readFile(path.resolve('./todos.json)));
+// }
+//
+// of course, this is mostly useful when you actually have lots of things to process
+// asynchronously :-)
+const todos = JSON.parse(fs.readFileSync(path.resolve("./todos.json")));
+
+app.get("/", (req, res) => res.json(todos));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
