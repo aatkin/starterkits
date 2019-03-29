0ab49a2e3453c8740a49593a08710c5238ee7335
Anssi Kinnunen <anssi.kinnunen01@gmail.com>
2019-03-29 10:17:24 +0200

    Remove cruft

--- redux-example/src/index.js
+++ redux-example/src/index.js
@@ -6,7 +6,6 @@ import { createStore } from "redux";
 import todoApp from "./reducers";
 import AppComponent from "./App";
 import "./index.css";
-import * as serviceWorker from "./serviceWorker";
 
 const store = createStore(todoApp);
 
@@ -16,8 +15,3 @@ ReactDOM.render(
   </Provider>,
   document.getElementById("root")
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
